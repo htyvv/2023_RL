3714598785dc9224ae4129f45bf2ac595a2df314
Taeyeong <htyvv@naver.com>
2023-01-30 23:12:46 +0900

    feat: Add sample game for rl study (w/ openAI gym)

--- frozenlake.py
+++ frozenlake.py
@@ -1,9 +1,46 @@
-import gym
+import gym 
+from gym.envs.registration import register
+import msvcrt
 
-env = gym.make("FrozenLake-v1")
-observation = env.reset()
+def get_key_in_str():
+    return msvcrt.getch().decode()
 
-for _ in range(1000):
+# key mapping
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+arrow_keys = {
+    'w' : UP,
+    's' : DOWN,
+    'd' : RIGHT,
+    'a' : LEFT
+}
+
+register(
+    id = 'FrozenLake-v3', 
+    entry_point='gym.envs.toy_text:FrozenLakeEnv', 
+    kwargs={
+        'map_name' : '4x4',
+        'is_slippery' : False
+    }
+)
+
+env = gym.make("FrozenLake-v3", render_mode="rgb_array")
+env.reset()
+env.render()
+
+while True:
+    key = get_key_in_str()
+    if key not in arrow_keys.keys():
+        print("Wrong key... Game aborted!")
+        break
+    action = arrow_keys[key]
+    state, reward, done, _, info = env.step(action)
     env.render()
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
+    print(f"State : {state}, Action : {action}, Reward : {reward}, Info : {info}")
+
+    if done :
+        print(f"Finished with reward {reward}")
+        break
